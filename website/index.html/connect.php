2e308d84b302eae1251998132affe19cbab5361c
Apurvadevelope <148629437+Apurvadevelope@users.noreply.github.com>
2023-10-28 02:10:56 +0530

    index

---
+++ website/index.html/connect.php
@@ -0,0 +1,22 @@
+
+
+
+
+<div  class="container20">
+
+    
+     
+      
+<video id="Test_Video" width="100%"
+        controls>
+    <source id="mp4_source"
+            src="video.MP4"
+            type="video/mp4">
+    <source id="ogg_source"
+            src="sample2.ogg"
+            type="video/ogg">
+</video>
+
+
+
+</div>
---
+++ website/index.html/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<div id="form">
+
+    <h1>Login Form</h1>
+    <form name="form" method="Post">
+        <label>Username:</label>
+        <input type="text" id="user" name="user">
+        <label>Password:</label>
+        <input type="password" id="pass" name="pass">
+        
+
+</div>
+    
+</body>
+</html>
